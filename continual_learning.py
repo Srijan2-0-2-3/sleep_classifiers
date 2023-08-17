da2b9942fc12b60f0d12c9473d951350fcb450e1
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2023-08-17 07:49:50 +0530

    changes for avalanche

--- continual_learning.py
+++ continual_learning.py
@@ -6,7 +6,7 @@ from source.preprocessing.raw_data_processor import RawDataProcessor
 def create_csv():
     SubjectBuilder.get_subject_dictionary()
 
-def load_csv():
+def update_csv():
     subject_ids = SubjectBuilder.get_all_subject_ids()
     for subject_id in subject_ids:
         print(subject_id)
@@ -14,9 +14,17 @@ def load_csv():
         print(df.head())
         print(df.shape)
         print(df.dtypes)
-        # df['count'] = df['count'].astype(float)
-        df = df.apply(pd.to_numeric)
+        columns = ['count', 'hr', 'time', 'cosine', 'sleep_label']
+        for column in columns:
+            df[column] = df[column].apply(lambda x: str(x.split()[0].replace('[', '')))
+            df[column] = df[column].apply(lambda x: str(x.split()[0].replace(']', '')))
+            df[column] = df[column].astype(float)
+        print(df.head())
+        df.to_csv(f'{subject_id}.csv')
+        # df = df.apply(pd.to_numeric)
         print(df.dtypes)
 
+
+
 if __name__=='__main__':
-    create_csv()
+    update_csv()
