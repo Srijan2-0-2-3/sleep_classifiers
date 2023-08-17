a8400ac82a44296ae43a8aeb37801509ff19764c
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2023-08-17 08:06:00 +0530

    changes for avalanche

--- continual_learning.py
+++ continual_learning.py
@@ -1,16 +1,18 @@
 import pandas as pd
-
+import numpy as np
 from source.analysis.setup.subject_builder import SubjectBuilder
 from source.preprocessing.raw_data_processor import RawDataProcessor
 
+
 def create_csv():
     SubjectBuilder.get_subject_dictionary()
+
 
 def update_csv():
     subject_ids = SubjectBuilder.get_all_subject_ids()
     for subject_id in subject_ids:
         print(subject_id)
-        df = pd.read_csv(f'{subject_id}.csv',index_col=0)
+        df = pd.read_csv(f'{subject_id}.csv', index_col=0)
         print(df.head())
         print(df.shape)
         print(df.dtypes)
@@ -25,6 +27,22 @@ def update_csv():
         print(df.dtypes)
 
 
+def load_csv():
+    subject_ids = SubjectBuilder.get_all_subject_ids()
+    for subject_id in subject_ids:
+        df = pd.read_csv(f'{subject_id}.csv', index_col=0)
+        print(df.columns)
+        print(df.shape)
+        array = df.to_numpy()
+        X = np.concatenate([array[0], array[1], array[2], array[3]])
+        print(X)
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+        print(f'{subject_id} standardised')
+        Y = df['sleep_label']
+        Y = np.around(Y)
+        Y = abs(Y.astype(np.int_))
+        print(f'{subject_id} labelled')
 
-if __name__=='__main__':
-    update_csv()
+
+if __name__ == '__main__':
+    load_csv()
