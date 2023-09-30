cc30432ccfdca6be9e500ef4e9d112763b7469d5
Srijan2-0-2-3 <2022proj037@hyderabad.bits-pilani.ac.in>
2023-09-30 13:39:24 +0530

    completed code for avalanche

--- graph plots.py
+++ graph plots.py
@@ -33,7 +33,7 @@ for key in methods.keys():
         methods[key]['disk_usage'].append(data[i]['disk_usage'])
         methods[key]['forg'].append(data[i]['forg'])
 
-
+traverse_dict_and_lists(methods['replay']['forg'])
 exp = [i for i in range(0, 30)]
 plt.plot(exp, methods['cumulative']['loss'], label='cumulative')
 plt.plot(exp, methods['replay']['loss'], label='replay')
@@ -45,110 +45,3 @@ plt.grid()
 plt.show()
 plt.savefig('loss_sl.png')
 
-# loss = []
-# cpu_usage = []
-# acc = []
-# forg = []
-# disk_usage = []
-# time = []
-#
-#
-# datas = pickle.load(open('replay_' + str(29) + '_sleep_classifier.pkl', 'rb'), encoding='latin1')
-# traverse_dict_and_lists(datas)
-# for i in range(0, 30):
-#     loss.append(datas[i]['loss'])
-#     acc.append(datas[i]['acc'])
-#     forg.append(datas[i]['forg'])
-#     cpu_usage.append(datas[i]['cpu_usage'])
-#     disk_usage.append(datas[i]['disk_usage'])
-#
-#
-#
-# methods['replay']['loss'] = loss
-# methods['replay']['acc'] = acc
-# methods['replay']['forg'] = forg
-# methods['replay']['cpu_usage'] = cpu_usage
-# methods['replay']['disk_usage'] = disk_usage
-# print(len(methods['replay']['loss']))
-# # plt.plot(exp, disk_usage)
-# # plt.savefig('disk_usage_sl.png')
-#
-# loss = []
-# cpu_usage = []
-# acc = []
-# forg = []
-# disk_usage = []
-# time = []
-#
-# datas = pickle.load(open('cumulative_' + str(29) + '_sleep_classifier.pkl', 'rb'), encoding='latin1')
-# print(datas)
-#
-# for i in range(0, 30):
-#     loss.append(datas[i]['loss'])
-#     acc.append(datas[i]['acc'])
-#     forg.append(datas[i]['forg'])
-#     cpu_usage.append(datas[i]['cpu_usage'])
-#     disk_usage.append(datas[i]['disk_usage'])
-#
-#
-#
-# methods['cumulative']['loss'] = loss
-# methods['cumulative']['acc'] = acc
-# methods['cumulative']['forg'] = forg
-# methods['cumulative']['cpu_usage'] = cpu_usage
-# methods['cumulative']['disk_usage'] = disk_usage
-# print(len(methods['cumulative']['loss']))
-# loss = []
-# cpu_usage = []
-# acc = []
-# forg = []
-# disk_usage = []
-# time = []
-# datas = pickle.load(open('lwf_' + str(29) + '_sleep_classifier.pkl', 'rb'), encoding='latin1')
-# print(datas)
-# for i in range(0, 30):
-#     loss.append(datas[i]['loss'])
-#     acc.append(datas[i]['acc'])
-#     forg.append(datas[i]['forg'])
-#     cpu_usage.append(datas[i]['cpu_usage'])
-#     disk_usage.append(datas[i]['disk_usage'])
-#
-# methods['lwf']['loss'] = loss
-# methods['lwf']['acc'] = acc
-# methods['lwf']['forg'] = forg
-# methods['lwf']['cpu_usage'] = cpu_usage
-# methods['lwf']['disk_usage'] = disk_usage
-# print(len(methods['lwf']['loss']))
-# m = methods['lwf']['loss'][0:30]
-
-#
-# figure, axis = plt.subplots(2, 3)
-#
-# axis[0, 0].plot(exp, methods['cumulative']['loss'],label = 'cumulative')
-# axis[0, 0].plot(exp, methods['replay']['loss'],label='replay')
-# axis[0, 0].plot(exp, methods['lwf']['loss'],label='lwf')
-# axis[0, 0].set_title("Loss")
-#
-# axis[0, 1].plot(exp, methods['cumulative']['acc'],label = 'cumulative')
-# axis[0, 1].plot(exp, methods['replay']['acc'],label='replay')
-# axis[0, 1].plot(exp, methods['lwf']['acc'],label='lwf')
-# axis[0, 1].set_title("Accuracy")
-#
-# axis[1, 0].plot(exp, methods['cumulative']['forg'],label = 'cumulative')
-# axis[1, 0].plot(exp, methods['replay']['forg'],label='replay')
-# axis[1, 0].plot(exp, methods['lwf']['forg'],label='lwf')
-# axis[1, 0].set_title("Forgetting")
-#
-# axis[1, 1].plot(exp, methods['cumulative']['cpu_usage'],label = 'cumulative')
-# axis[1, 1].plot(exp, methods['replay']['cpu_usage'],label='replay')
-# axis[1, 1].plot(exp, methods['lwf']['cpu_usage'],label='lwf')
-# axis[1, 1].set_title("Cpu usage")
-#
-# axis[1, 2].plot(exp, methods['cumulative']['disk_usage'],label = 'cumulative')
-# axis[1, 2].plot(exp, methods['replay']['disk_usage'],label='replay')
-# axis[1, 2].plot(exp, methods['lwf']['disk_usage'],label='lwf')
-# axis[1, 2].set_title("disk usage")
-#
-# # Combine all the operations and display
-# plt.show()
-# plt.savefig('sleep_classifier.png')
